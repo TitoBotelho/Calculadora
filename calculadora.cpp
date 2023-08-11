e2b3a612c9c255dc14afb8649c87ed87557e2411
TitoBotelho <103445424+TitoBotelho@users.noreply.github.com>
2023-08-11 13:41:18 -0300

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char operador;
+    double num1, num2;
+
+    printf("Digite um operador (+, -, *, /): ");
+    scanf("%c", &operador);
+
+    printf("Digite dois números: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operador) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            } else {
+                printf("Não é possível dividir por zero.\n");
+            }
+            break;
+        default:
+            printf("Operador inválido.\n");
+    }
+
+    return 0;
+}
